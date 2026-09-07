f19badeab1b8dd1202e52c7ba461a0f2576ecf04
Nauman188 <noumanasif00@gmail.com>
2026-09-07 13:01:51 +0500

    add Google Search Console verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,11 @@ import AuthProvider from "@/components/AuthProvider";
 export const metadata = {
   title: "PrimeIndexer Clone - SaaS App",
   description: "Fast link indexing SaaS application",
+  verification: {
+    google: "9QFHfmFeKS2Rsp0Be_dpM_jann8ZwKLmUKvczGHHMEE",
+  },
 };
+
 
 export default function RootLayout({
   children,
